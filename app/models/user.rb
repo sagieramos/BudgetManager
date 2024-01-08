7590ab13185c31e315c442b20d6ef1a6cd294536
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-01-08 18:15:29 +0100

    Add devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
-    has_many :entities, dependent: :destroy
-    has_many :groups, dependent: :destroy
-  
-    before_destroy :delete_associated_group_entities
-  
-    private
-  
-    def delete_associated_group_entities
-      groups.each do |group|
-        group.group_entities.destroy_all
-      end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :entities, dependent: :destroy
+  has_many :groups, dependent: :destroy
+
+  before_destroy :delete_associated_group_entities
+
+  private
+
+  def delete_associated_group_entities
+    groups.each do |group|
+      group.group_entities.destroy_all
+    end
+
+    entities.each do |entity|
+      entity.group_entities.destroy_all
     end
   end
-  
+end
