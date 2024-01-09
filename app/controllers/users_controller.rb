bfefea5e726db7865aa9465b8d4b10237f4b6b12
Stanley Osagie Ramos <sagiecyber@gmail.com>
2024-01-09 08:25:45 +0100

    fix registration params permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  rescue_from ActionController::InvalidAuthenticityToken, with: :handle_unauthorized
+
+  def index
+    @users = current_user.entities
+  end
+
+  private
+
+  def handle_unauthorized
+    flash[:warning] = 'You are not authorized to access this resource. Please sign in.'
+    redirect_to new_user_session_path
+  end
 end
